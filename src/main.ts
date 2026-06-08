64dec4524fbfee7baced2de496b19ea5e5f84159
romeusclarens <romeusclarens10@gmail.com>
2026-06-08 17:43:11 -0400

    fix: cors issue 2

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     'http://localhost:5173',
     'http://localhost:5174',
     'https://unisys.cariblink.org',
-    'https://https://www.cariblink.org',
+    'https://www.cariblink.org',
   ];
 
 app.use(helmet());
